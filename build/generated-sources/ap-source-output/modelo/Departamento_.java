6b77ea8a3d16dc55936793ee64a53b71dc072732
Gilson123tenemea <tenemeaaguilar@gmail.com>
2025-05-18 14:27:18 -0500

    Primer commit

---
+++ build/generated-sources/ap-source-output/modelo/Departamento_.java
@@ -0,0 +1,17 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Empleados;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-17T22:54:23")
+@StaticMetamodel(Departamento.class)
+public class Departamento_ { 
+
+    public static volatile ListAttribute<Departamento, Empleados> empleadosList;
+    public static volatile SingularAttribute<Departamento, Integer> id;
+    public static volatile SingularAttribute<Departamento, String> nombre;
+
+}
---
+++ build/generated-sources/ap-source-output/modelo/Empleados_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Departamento;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-17T22:54:23")
+@StaticMetamodel(Empleados.class)
+public class Empleados_ { 
+
+    public static volatile SingularAttribute<Empleados, Integer> idEmpl;
+    public static volatile SingularAttribute<Empleados, Departamento> id;
+    public static volatile SingularAttribute<Empleados, String> nombre;
+
+}
